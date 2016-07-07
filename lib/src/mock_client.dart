d222975809e57faf61eb38c7ebaba6301bbd625e
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2016-07-07 16:46:05 +0200

    add debug attributes to MockJSONAPIClient class

--- lib/src/mock_client.dart
+++ lib/src/mock_client.dart
@@ -10,6 +10,18 @@ import 'document.dart';
 class MockJSONAPIClient implements JSONAPIClient {
   var request; // unused
 
+  String _url;
+  String get requestUrl => _url;
+
+  String _payload;
+  String get requestPayload => _payload;
+
+  List<String> _includeModels;
+  List<String> get requestIncludedModels => _includeModels;
+
+  Map _headers;
+  Map get requestHeaders => _headers;
+
   var _desiredOutput;
 
   setOutput(dynamic output){
@@ -17,16 +29,31 @@ class MockJSONAPIClient implements JSONAPIClient {
   }
 
   Future<JSONAPIDocument> get(String url,
-      {List<String> includeModels, Map headers}) async {
+      {List<String> includeModels: null, Map headers: null}) async {
+        _url = url;
+        _payload = null;
+        _includeModels = includeModels;
+        _headers = headers;
+
         return new Future.value(_desiredOutput);
       }
 
-  Future<JSONAPIDocument> post(String url, String document,
+  Future<JSONAPIDocument> post(String url, String payload,
     {List<String> includeModels, Map headers}) async {
+      _url = url;
+      _payload = payload;
+      _includeModels = includeModels;
+      _headers = headers;
+
       return new Future.value(_desiredOutput);
     }
 
   Future delete(String url, {Map headers}) async {
+    _url = url;
+    _payload = null;
+    _includeModels = null;
+    _headers = headers;
+
     return new Future.value(_desiredOutput);
   }
 }
